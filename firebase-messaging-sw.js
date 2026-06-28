485ee24a58a0ac52f3d682a91802563606b6f0ce
mazinramos <mazenramos0@gmail.com>
2026-06-28 16:09:01 +0300

    Upgrade Firebase SDK to version 8.10.1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,26 @@
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
-  
-const firebaseConfig = {
-  apiKey: "AIzaSyCtQtSikYfWEPtKjwxVIJim8fHzeW5ACtY",
-  authDomain: "gess-platform.firebaseapp.com",
-  projectId: "gess-platform",
-  storageBucket: "gess-platform.firebasestorage.app",
-  messagingSenderId: "794482265366",
-  appId: "1:794482265366:web:0108b0cae375d8efd3d437",
-  measurementId: "G-JGMGBK6CVW"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: "AIzaSyCtQtSikYfWEPtKjwxVIJim8fHzeW5ACtY",
+    authDomain: "gess-platform.firebaseapp.com",
+    projectId: "gess-platform",
+    storageBucket: "gess-platform.firebasestorage.app",
+    messagingSenderId: "794482265366",
+    appId: "1:794482265366:web:0108b0cae375d8efd3d437"
+});
 
 const messaging = firebase.messaging();
 
+messaging.setBackgroundMessageHandler(function(payload) {
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('Received background message ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/logo.png',  
-        badge: '/logo.png'
-    };
+    return self.registration.showNotification(
+        payload.notification.title,
+        {
+            body: payload.notification.body,
+            icon: "/logo.png",
+            badge: "/logo.png"
+        }
+    );
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
 });
